02ea9744178dc73b27d44dc816e7224d731dd87a
高鹏 <gaopeng_zh@scm.cfets.com>
2018-10-31 15:24:44 +0800

    update

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, NewTable, Product
+from .models import Post, NewTable, Product, PModel, Maker
 
 # Register your models here.
 
@@ -8,10 +8,20 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'pub_date')
 
 
+class MakerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+
+
+class PModelAdmin(admin.ModelAdmin):
+    list_display = ('maker', 'name', 'url')
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'qty')
+    list_display = ('nickname', 'price', 'description')
 
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(NewTable)
+admin.site.register(Maker, MakerAdmin)
+admin.site.register(PModel, PModelAdmin)
 admin.site.register(Product, ProductAdmin)
--- mainsite/models.py
+++ mainsite/models.py
@@ -29,17 +29,38 @@ class NewTable(models.Model):
     text_f = models.TextField()
 
 
-class Product(models.Model):
-    # SIZES = (
-    #     ('S', 'Small'),
-    #     ('M', 'Medium'),
-    #     ('L', 'Large'),
-    # )
-    sku = models.CharField(max_length=5)
-    name = models.CharField(max_length=20)
-    price = models.PositiveIntegerField()
-    # size = models.CharField(max_length=1, choices=SIZES)
-    qty = models.IntegerField(default=0)
+class Maker(models.Model):
+    name = models.CharField(max_length=10)
+    country = models.CharField(max_length=10)
 
     def __unicode__(self):
         return self.name
+
+
+class PModel(models.Model):
+    maker = models.ForeignKey(Maker, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    url = models.URLField(default='http://i.imgur.com/Ous4iGB.png')
+
+    def __unicode__(self):
+        return self.name
+
+
+class Product(models.Model):
+    pmodel = models.ForeignKey(PModel, on_delete=models.CASCADE, null=True)
+    nickname = models.CharField(max_length=15, default='超值二手机')
+    description = models.TextField(default='暂无说明')
+    year = models.PositiveIntegerField(default=2018)
+    price = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return self.nickname
+
+
+class PPhoto(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.CharField(max_length=20, default='产品照片')
+    url = models.URLField(default='http://i.imgur.com/Z230eeq.png')
+
+    def __unicode__(self):
+        return self.description
